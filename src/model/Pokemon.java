217f338392f7fb1f3b92971bbf278142ece35f32
souru98 <kulkarnisourabh1998@gmail.com>
2020-03-04 23:17:00 +0530

    my code

--- src/model/Pokemon.java
+++ src/model/Pokemon.java
@@ -8,3 +8,33 @@ package model;
 //Generate appropriate getters and setters.
 //Generate a two-argument constructor in the Pokemon class
 
+//parent class
+ public  class Pokemon
+{
+	 //arguments
+	  int pokemonNumber;
+	  String pokemonName;
+	  
+	  
+	//getter and setter
+	public String getPokemonName() {
+		return pokemonName;
+	}
+	public void setPokemonName(String pokemonName) {
+		this.pokemonName = pokemonName;
+	}
+	public int getPokemonNumber() {
+		return pokemonNumber;
+	}
+	public void setPokemonNumber(int pokemonNumber) {
+		this.pokemonNumber = pokemonNumber;
+	}
+	
+	//constructor
+	Pokemon(String pokemonName,int pokemonNumber)
+	{
+		this.pokemonName=pokemonName;
+		this.pokemonNumber=pokemonNumber;
+		
+	}
+}
