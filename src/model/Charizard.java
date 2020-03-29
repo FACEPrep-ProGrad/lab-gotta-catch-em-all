7f343d22fdd188dc75d1253f9158175dee4b9950
jwala5 <dubeyshiva98@gmail.com>
2020-03-29 21:23:59 +0530

    FSJ0320A004

--- src/model/Charizard.java
+++ src/model/Charizard.java
@@ -1,6 +1,9 @@
 package model;
 
 
+package model;
+
+
 //PROGRESSION - 3
 //Go to JAVA Resouces - src/model and create a child class called Charizard inside the model package with the following arguments
 //String characteristics 
@@ -19,3 +22,62 @@ package model;
 //type = "FIRE"
 //evolution = "CHARMELEON"
 //baseExp = "240" 
+
+
+public class Charizard  extends Pokemon
+{
+	
+	//arguments
+	String characteristics;
+	String type;
+	String evolution;
+	String baseExp;
+	
+	/*constructor */
+		public Charizard (String pokemonName, int pokemonNumber, String characteristics, String type, String evolution,
+				String baseExp)
+		{
+			super(pokemonName, pokemonNumber);
+			this.characteristics = characteristics;
+			this.type = type;
+			this.evolution = evolution;
+			this.baseExp = baseExp;
+		}
+	
+	
+//getter and setter//
+	public String getCharacteristics() {
+		return characteristics;
+	}
+
+	public void setCharacteristics(String characteristics) {
+		this.characteristics = characteristics;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getEvolution() {
+		return evolution;
+	}
+
+	public void setEvolution(String evolution) {
+		this.evolution = evolution;
+	}
+
+	public String getBaseExp() {
+		return baseExp;
+	}
+
+	public void setBaseExp(String baseExp) {
+		this.baseExp = baseExp;
+	}
+	
+	
+	
+}
