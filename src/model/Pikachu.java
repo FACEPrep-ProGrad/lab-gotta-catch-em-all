d000bed130a18f4d09d4d4ef8d5a7b8c19d76632
Ravikachhadiya <ravikachhadiya@gmail.com>
2020-09-08 17:32:24 +0530

    FSJ0720A115

--- src/model/Pikachu.java
+++ src/model/Pikachu.java
@@ -1,5 +1,56 @@
 package model;
 
+
+public class Pikachu extends Pokemon{
+	
+	String characteristics;
+	String type;
+	String evolution;
+	String baseExp;
+	public Pikachu(String pokemonName, int pokemonNumber, String characteristics, String type, String evolution,
+			String baseExp) {
+		super(pokemonName, pokemonNumber);
+		this.characteristics = characteristics;
+		this.type = type;
+		this.evolution = evolution;
+		this.baseExp = baseExp;
+	}
+	
+	public Pikachu() {
+		super("Pikachu", 25);
+		this.characteristics = "Pikachu is an Electric-type Pokémon, which was introduced in Generation I. Over the years, Pikachu has become so popular that it serves as the Pokémon franchise mascot.";
+		this.type = "ELECTRIC" ;
+		this.evolution = "RAICHU";
+		this.baseExp = "112";
+	}
+	public String getCharacteristics() {
+		return characteristics;
+	}
+	public void setCharacteristics(String characteristics) {
+		this.characteristics = characteristics;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getEvolution() {
+		return evolution;
+	}
+	public void setEvolution(String evolution) {
+		this.evolution = evolution;
+	}
+	public int getBaseExp() {
+		return Integer.parseInt(baseExp);
+	}
+	public void setBaseExp(String baseExp) {
+		this.baseExp = baseExp;
+	}
+	
+	
+}
+
 /*
  * PROGRESSION - 6 
  * Go to JAVA Resouces - src/model and create a child class
--- src/model/Pokemon.java
+++ src/model/Pokemon.java
@@ -1,5 +1,34 @@
 package model;
 
+
+public class Pokemon{
+	String pokemonName;
+	int pokemonNumber;
+	
+	public Pokemon(String pokemonName, int pokemonNumber) {
+		this.pokemonName = pokemonName;
+		this.pokemonNumber = pokemonNumber;
+	}
+
+	public String getPokemonName() {
+		return pokemonName;
+	}
+
+	public void setPokemonName(String pokemonName) {
+		this.pokemonName = pokemonName;
+	}
+
+	public int getPokemonNumber() {
+		return pokemonNumber;
+	}
+
+	public void setPokemonNumber(int pokemonNumber) {
+		this.pokemonNumber = pokemonNumber;
+	}
+	
+	
+}
+
 //PROGRESSION - 1 
 //Go to JAVA Resouces - src/model and create a parent class called Pokemon inside the model package - with the following arguments
 //String pokemonName
