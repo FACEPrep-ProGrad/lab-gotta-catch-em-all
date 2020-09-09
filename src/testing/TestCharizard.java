a01bf11979b1328a5090992137f6cfd8abcd95de
surajdesai1297@gmail.com <suraj@LAPTOP-C83DOH9J.localdomain>
2020-09-09 20:03:46 +0530

    FSJ0820B107

--- src/testing/TestCharizard.java
+++ src/testing/TestCharizard.java
@@ -1,6 +1,6 @@
 package testing;
 
-/*
+
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
@@ -15,9 +15,9 @@ public class TestCharizard {
 		String temp = "Charizard is fit and strong and has the ability to soar up to ridiculous heights. With a single breath of fire, it can burn down forests and melt gigantic glaciers.";
 		assertEquals(temp,p.getCharacteristics());		
 		try {
-			Charizard p = new Charizard(null,0,null,null,null,null);
+			Charizard p1 = new Charizard(null,0,null,null,null,null);
 			temp = null;
-			bulb.getCharacteristics();
+			p1.getCharacteristics();
 		}
 		catch(Exception e) {
 			e.printStackTrace();
@@ -30,7 +30,7 @@ public class TestCharizard {
 		String temp = "FIRE";
 		assertEquals(temp,p.getType());		
 		try {
-			Charizard p = new Charizard(null,0,null,null,null,null);
+			Charizard p1 = new Charizard(null,0,null,null,null,null);
 			temp = null;
 			p1.getType();
 		}
@@ -45,7 +45,7 @@ public class TestCharizard {
 		String temp = "CHARMELEON";
 		assertEquals(temp,p.getEvolution());		
 		try {
-			Charizard p = new Charizard(null,0,null,null,null,null);
+			Charizard p1 = new Charizard(null,0,null,null,null,null);
 			temp = null;
 			p1.getEvolution();
 		}
@@ -56,11 +56,11 @@ public class TestCharizard {
 	@Test
 	public void testBaseExpMethod() {
 		Charizard p = new Charizard("AAA",4,"Charizard is fit and strong and has the ability to soar up to ridiculous heights. With a single breath of fire, it can burn down forests and melt gigantic glaciers.","FIRE","CHARMELEON", "240" );
-		int temp = 240;
+		String temp = "240";
 		assertEquals(temp,p.getBaseExp());		
 		try {
-			Charizard p = new Charizard(null,0,null,null,null,null);
-			temp = 0;
+			Charizard p1 = new Charizard(null,0,null,null,null,null);
+			temp = null;
 			p1.getBaseExp();
 		}
 		catch(Exception e) {
@@ -68,4 +68,3 @@ public class TestCharizard {
 		}
 	}
 }
-*/
